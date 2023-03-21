693cd1e7608aa33e865396f648a82beb41148cf0
UtopicPanther <upanther@furrygames.top>
2023-03-21 19:46:36 +0800

    libfrt/i18n: impl std::fmt::Display for LangId

--- crates/libfrt/src/i18n.rs
+++ crates/libfrt/src/i18n.rs
@@ -77,3 +77,9 @@ impl LangId {
         self.tag().0
     }
 }
+
+impl std::fmt::Display for LangId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
